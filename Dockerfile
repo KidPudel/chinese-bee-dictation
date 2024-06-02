0e6fd13279017199d1b8ae13b9f044f01e2d6602
Iggy Kuperson <i.kupchinenko@gmail.com>
2024-06-02 16:16:00 +0300

    docker image with nginx

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# build stage (pre-requisite)
+FROM node:21.6.1-alpine AS build-stage
+
+ENV NODE_ENV=production
+
+WORKDIR /app
+
+# cache dependencies (what won't change often)
+COPY package*.json /.
+
+RUN npm install
+
+COPY . .
+
+
+# serve stage
+FROM nginx:stable-alpine AS production-stage
+
+# where dist is where build static files are located
+COPY --from=build-stage /app/dist /usr/share/nginx/html
+
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,113 @@
 <template>
   <div id="desk">
-    <h1>Chinese Bee Dictation</h1>
-    <vue-drawing-canvas ref="canvas" :width="500" />
-    <button @click="clear">Отчистить</button>
-    
+    <h2>{{ question }}</h2>
+    <vue-drawing-canvas
+      ref="canvas"
+      :lineJoin="'round'"
+      :lineCap="'round'"
+      :width="250"
+      :height="250"
+      v-model:image="characterImage"
+    />
+    <div>
+      <button @click="clear">Отчистить</button>
+      <button @click="rate">Проверить</button>
+    </div>
+    <button @click="next">Следующее слово</button>
+    <h2 v-if="score != 0">{{ score }}</h2>
   </div>
 </template>
 
 <script>
-import VueDrawingCanvas from "vue-drawing-canvas"
+import VueDrawingCanvas from "vue-drawing-canvas";
 
 export default {
   name: "App",
   components: {
     VueDrawingCanvas,
-  }
-}
+  },
+};
 </script>
-
 
 <script setup>
-import {ref, onMounted} from 'vue'
+import { ref, onMounted } from "vue";
+import axios from "axios";
+import qs from "qs";
 
-const canvas = ref(null)
+const canvas = ref(null);
+
+const characterImage = defineModel();
+
+const question = ref("")
+const rightAnswer = ref("")
+const currentIndex = ref(0)
+
+const characters = ref([])
+
+const score = ref(0)
 
 onMounted(() => {
-  canvas = this.$refs.canvas
-})
+  console.log("AAAAAAAAA")
+  
+  const options = {
+    method: "GET",
+    url: "http://localhost:8000/saved-words?user_id=1998388306",
+  };
+  axios(options).then((response) => {
+    if (response.data["success"] == true) {
+      characters.value = response.data["saved_words"]
+      question.value = characters.value[currentIndex.value]["russian"]
+      rightAnswer.value = characters.value[currentIndex.value]["chinese"] 
+      console.log(characters.value)
+    } else {
+      console.log(response)
+    }
+  }); 
+});
 
 function clear() {
-  canvas.value.reset()
+  canvas.value.reset();
 }
 
+function rate() {
+  score.value = "Обработка..."
+  const options = {
+    method: "POST",
+    header: { "content-type": "application/x-www-form-urlencoded" },
+    data: qs.stringify({ image: characterImage.value, target: rightAnswer.value }),
+    url: "http://localhost:8000/accuracy-score",
+  };
+  axios(options).then((response) => {
+    console.log(response.data)
+    if (response.data["success"] == true) {
+      if (response.data["score"] == 0) {
+        console.log("set score to string")
+        score.value = "0%, " + question.value + " - " + rightAnswer.value
+      } else {
+        score.value = response.data["score"] * 100 + "%"
 
+      }
+    }
+  });
+}
 
+function next() {
+  score.value = 0
+  currentIndex.value++
+  if (currentIndex.value == characters.value.length) {
+    currentIndex.value = 0
+  }
+  question.value = characters.value[currentIndex.value]["russian"]
+  rightAnswer.value =characters.value[currentIndex.value]["chinese"]  
+}
 
-</script>
+</script> 
 
 <style>
+* {
+  font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+
+}
 :root {
   width: 100vw;
   height: 100vh;
@@ -45,6 +115,7 @@ function clear() {
   display: flex;
   justify-content: center;
   align-items: center;
+  font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
 }
 #desk {
   height: 80vh;
@@ -62,5 +133,87 @@ canvas {
   display: inline-block;
   box-shadow: 0 2px 5px grey;
   border-radius: 5px;
+  margin: .25rem;
 }
+
+button {
+  background-color: #fff;
+  background-image: none;
+  background-position: 0 90%;
+  background-repeat: repeat no-repeat;
+  background-size: 4px 3px;
+  border-radius: 15px 225px 255px 15px 15px 255px 225px 15px;
+  border-style: solid;
+  border-width: 2px;
+  box-shadow: rgba(0, 0, 0, .2) 15px 28px 25px -18px;
+  box-sizing: border-box;
+  outline: none;
+  padding: .35rem;
+  margin: .10rem;
+  transition: all 235ms ease-in-out;
+  touch-action: manipulation;
+  border-bottom-left-radius: 15px 255px;
+  border-bottom-right-radius: 225px 15px;
+  border-top-left-radius: 255px 15px;
+  border-top-right-radius: 15px 225px;
+  font-weight: bold;
+  color: #41403e;
+
+  user-select: none;
+  -webkit-user-select: none;
+}
+
+button:hover {
+  box-shadow: rgba(0, 0, 0, .3) 2px 8px 8px -5px;
+  transform: translate3d(0, 2px, 0);
+}
+
+button:focus {
+  box-shadow: rgba(0, 0, 0, .3) 2px 8px 4px -6px;
+}
+
 </style>
+
+
+<!-- HTML !-->
+
+/* CSS */
+.button-55 {
+  align-self: center;
+  background-color: #fff;
+  background-image: none;
+  background-position: 0 90%;
+  background-repeat: repeat no-repeat;
+  background-size: 4px 3px;
+  border-radius: 15px 225px 255px 15px 15px 255px 225px 15px;
+  border-style: solid;
+  border-width: 2px;
+  box-shadow: rgba(0, 0, 0, .2) 15px 28px 25px -18px;
+  box-sizing: border-box;
+  color: #41403e;
+  cursor: pointer;
+  display: inline-block;
+  font-family: Neucha, sans-serif;
+  font-size: 1rem;
+  line-height: 23px;
+  outline: none;
+  padding: .75rem;
+  text-decoration: none;
+  transition: all 235ms ease-in-out;
+  border-bottom-left-radius: 15px 255px;
+  border-bottom-right-radius: 225px 15px;
+  border-top-left-radius: 255px 15px;
+  border-top-right-radius: 15px 225px;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+}
+
+.button-55:hover {
+  box-shadow: rgba(0, 0, 0, .3) 2px 8px 8px -5px;
+  transform: translate3d(0, 2px, 0);
+}
+
+.button-55:focus {
+  box-shadow: rgba(0, 0, 0, .3) 2px 8px 4px -6px;
+}
