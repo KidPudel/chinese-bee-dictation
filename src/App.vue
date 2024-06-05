6e84fe2e0da0561c0f626cfcc64905bd7d1bec2d
Iggy Kuperson <i.kupchinenko@gmail.com>
2024-06-05 19:50:14 +0300

    allow origin everywhere

--- src/App.vue
+++ src/App.vue
@@ -80,8 +80,10 @@ function clear() {
 function rate() {
   score.value = "Обработка..."
   const options = {
+    
     method: "POST",
-    header: { "content-type": "application/x-www-form-urlencoded" },
+    header: { "content-type": "application/x-www-form-urlencoded", "Accepts": "application/json",
+      "Access-Control-Allow-Origin": "*",  },
     data: qs.stringify({ image: characterImage.value, target: rightAnswer.value }),
     url: `${api}/accuracy-score`,
   };
