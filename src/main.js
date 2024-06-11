b9497eb13dd7dcc7fc2c09c4664266b2a0d1c5bc
Iggy Kuperson <i.kupchinenko@gmail.com>
2024-06-11 19:13:20 +0300

    setup router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import {createApp} from 'vue'
+import {createRouter} from 'vue-router'
 import App from './App.vue'
+
+const router = createRouter()
 
 const app = createApp(App)
 
 // bind
-app.mount("#app")
+app.use(router).mount("#app")
