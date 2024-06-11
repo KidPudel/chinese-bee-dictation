ca25dbe3fb4df45b72bb9efc6f5d84992d2b114a
Iggy Kuperson <i.kupchinenko@gmail.com>
2024-06-11 19:21:19 +0300

    fix routes

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import {createApp} from 'vue'
 import {createRouter} from 'vue-router'
 import App from './App.vue'
 
-const router = createRouter()
+const routes = [
+    {path: '/', component: App}
+]
+
+const router = createRouter(routes)
 
 const app = createApp(App)
 
